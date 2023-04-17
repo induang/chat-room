b3298cafca3d1cc838e9079e5e199ce59b030099
Dwan <yingduan_ge@epam.com>
2023-04-17 11:41:43 +0800

    add tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return <div className="w-24 bg-red-300 h-24">hello world</div>;
 }
 
 export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+/* 
+  还是得使用Command JS 才会生效
+*/
