0b67963aae9377affdbff0913b0ea835446138d0
Dwan <yingduan_ge@epam.com>
2023-04-21 18:41:57 +0800

    add axios handle http

---
+++ src/utils/httpRequest.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+
+const httpRequest = axios.create({
+	baseURL: 'http://localhost:5000',
+	timeout: 10000,
+	headers: {
+		"Content-Type": 'application/json;charset=utf-8',
+	}
+});
+
+httpRequest.interceptors.request.use(
+	(config) => {
+		const token = window.localStorage.getItem('token') || "";
+		if(config.headers){
+			config.headers.Authorization = token;
+		}
+		return config;
+	},
+	(error) => {
+		console.log('ying ',error)
+		Promise.reject(error);
+	}
+)
+
+httpRequest.interceptors.response.use(
+	(response) => {
+		const token = response.headers.Authorization;
+		token && window.localStorage.setItem('token', token);
+		return response;
+	},
+	(error) => {
+		const message = error.response?.data?.errorMessage || error.message;
+		if(error.response?.status === 401){
+			console.log("unauthenrization");
+			window.localStorage.removeItem('token');
+			window.location.href = '/.login'
+		}
+		return Promise.reject()
+	}
+)
+
+export default httpRequest;
